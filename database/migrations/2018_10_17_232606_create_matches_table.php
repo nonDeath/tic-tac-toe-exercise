b7b3204d1465fbf379c5d88aedc71c19954cb4d5
Marcelo <marceloaldo@gmail.com>
2018-10-17 21:06:01 -0300

    First commit with some changes XD!

---
+++ database/migrations/2018_10_17_232606_create_matches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('next');
+            $table->unsignedInteger('winner');
+            $table->string('board');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
